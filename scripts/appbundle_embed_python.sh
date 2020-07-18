b464530d1e3ecbf94691c6157b435ceb6d966000
connorbkirk <ckirk96@gmail.com>
2020-07-18 14:47:32 +0700

    add prints when bundling python framework macos

--- scripts/appbundle_embed_python.sh
+++ scripts/appbundle_embed_python.sh
@@ -17,12 +17,16 @@ mkdir -p "$appbundle/Contents/Frameworks" || exit 1
 cp -a "$py_framework" "$appbundle/Contents/Frameworks/" || exit 1
 
 echo "Making executable $executable point to embedded Framework"
-install_name_tool -change `otool -L "$executable" | sed -n "s/^[[:blank:]]*\([^[:blank:]]*Python\) (.*$/\1/p"` @executable_path/../Frameworks/Python.framework/Versions/Current/Python "$executable" 
+install_name_tool -change `otool -L "$executable" | sed -n "s/^[[:blank:]]*\([^[:blank:]]*Python\) (.*$/\1/p"` @executable_path/../Frameworks/Python.framework/Versions/Current/Python "$executable"
 
 echo "Cleaning up embedded Python Framework"
 cd "$appbundle/Contents/Frameworks/Python.framework" || exit 1
 find . | grep -E "(__pycache__|\.pyc|\.pyo$)" | xargs rm -rf || exit 1
+echo "\n---before---"
+ls "Versions/Current/Resources/"
 rm -r Versions/Current/Resources/* Versions/Current/lib/python3.6/test Versions/Current/lib/python3.6/idlelib Versions/Current/lib/python3.6/curses Versions/Current/lib/python3.6/lib2to3 || exit 1
+echo "\n---after---"
+ls "Versions/Current/Resources/"
 
 echo "Checking if PySide2 is available"
 
